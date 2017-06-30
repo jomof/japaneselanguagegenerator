ca834ac07091e80de8722cab7044c635b7e85ed9
Jomo Fisher (desktop) <jomofisher@gmail.com>
2017-06-30 09:44:18 -0700

    Add project files.

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,12 @@
+﻿
+#include "stdafx.h"
+
+
+int main()
+{
+	SetConsoleOutputCP(932);
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	std::wcout << L"Testing unicode -- English -- わとし -- Español." << std::endl;
+    return 0;
+}
+
---
+++ ConsoleApplication1/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <windows.h>
+#include <wincon.h>
+
+
+// TODO: reference additional headers your program requires here
